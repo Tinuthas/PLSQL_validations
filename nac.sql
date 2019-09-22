aeaf85a389b0a3b331d2a7162618a3663592e06c
tinuthas <vinicius1504_medeiros@hotmail.com>
2019-09-22 19:49:37 -0300

    cnpj feito

--- nac.sql
+++ nac.sql
@@ -17,6 +17,9 @@ VALUES
 INSERT INTO DOCUMENTOS
 VALUES
 (95643234701,345323567,43353453000186,3425342133);
+INSERT INTO DOCUMENTOS
+VALUES
+(12356621363,120300013,34534555000155,435687090612);
 commit;
 
 SELECT CPF,
@@ -29,34 +32,43 @@ FROM DOCUMENTOS;
 
 SET SERVEROUTPUT ON
 
+
 create or replace function VERIFICA_CPF(cpf documentos.cpf%type)
-RETURN VARCHAR2 AS
+RETURN VARCHAR2 IS
+    v_text VARCHAR2(255) := 'CPF VÁLIDO';
     TYPE v_array_type IS TABLE OF NUMBER INDEX BY PLS_INTEGER; 
     v_array_dv v_array_type;
     v_sum number :=0;
 BEGIN 
     IF (LENGTH(cpf) != 11) THEN
-        RETURN 'CPF INVÁLIDO';
-    END IF;
-    FOR i In 0..10 LOOP
-        v_array_dv(i -1) := TO_NUMBER(SUBSTR(cpf, i, 1));
-    END LOOP;
-    FOR i IN 0..9 LOOP
-    --((i+1) * v_array_dv(i))
-        v_sum := v_sum + ((i+1) *v_array_dv(i));
-        IF (i = 8 ) THEN
-            IF(MOD(v_sum, 11) != v_array_dv(9)) THEN
-                RETURN 'CPF INVÁLIDO';
+        v_text := 'CPF INVÁLIDO';
+    ELSE
+         FOR i In 0..11 LOOP
+            v_array_dv(i -1) := TO_NUMBER(SUBSTR(cpf, i, 1));
+        END LOOP;
+        FOR i IN 0..8 LOOP
+            IF (((i+1) * v_array_dv(i)) != 10) THEN 
+                 v_sum := v_sum + ((i+1) * v_array_dv(i));
             END IF;
-        ELSIF (i = 9) THEN
+         END LOOP;
+         IF(MOD(v_sum, 11) != v_array_dv(9)) THEN
+             v_text := 'CPF INVÁLIDO';
+         ELSE
+            v_sum := 0;
+             FOR i IN 0..9 LOOP
+                IF ((i * v_array_dv(i)) != 10) THEN 
+                     v_sum := v_sum + (i * v_array_dv(i));
+                END IF;
+            END LOOP;
             IF(MOD(v_sum, 11) != v_array_dv(10)) THEN
-                RETURN 'CPF SINVÁLIDO';
-            END IF;
-        END IF;
-    END LOOP;
-
-    RETURN 'CPF ';
-    
+                v_text := 'CPF_INVÁLIDO';
+             END IF;
+         END IF; 
+    END IF;
+    RETURN v_text;
+EXCEPTION
+  WHEN OTHERS THEN
+    RETURN 'OCORREU ALGUMA EXCEÇÃO';
     --MOD(v_sum_nine, 11)
     
 END VERIFICA_CPF;
@@ -65,3 +77,63 @@ END VERIFICA_CPF;
 SELECT CPF,
  VERIFICA_CPF(CPF)
 FROM DOCUMENTOS;
+
+
+
+create or replace function VERIFICA_CNPJ(cnpj documentos.cnpj%type)
+RETURN VARCHAR2 IS
+    v_text VARCHAR2(255) := 'CNPJ VÁLIDO';
+    TYPE v_array_type IS TABLE OF NUMBER INDEX BY PLS_INTEGER; 
+    v_array_dv v_array_type;
+    v_sum number :=0;
+    v_aux number := 0;
+BEGIN 
+    IF (LENGTH(cnpj) != 14) THEN
+        v_text := 'CNPJ INVÁLIDO';
+    ELSE
+         FOR i In 0..14 LOOP
+            v_array_dv(i -1) := TO_NUMBER(SUBSTR(cnpj, i, 1));
+        END LOOP;
+        v_aux := 6;
+        FOR i IN 0..11 LOOP
+            IF(v_aux = 10) THEN
+                v_aux := 2;
+            END IF;
+            IF ((v_aux * v_array_dv(i)) != 10) THEN 
+                 v_sum := v_sum + (v_aux * v_array_dv(i));
+            END IF;
+            v_aux := v_aux+ 1;
+         END LOOP;
+         IF(MOD(v_sum, 11) != v_array_dv(12)) THEN
+             v_text := 'CNPJ INVÁLIDO';
+         ELSE
+            v_sum := 0;
+            v_aux := 5;
+             FOR i IN 0..12 LOOP
+                IF(v_aux = 10) THEN
+                    v_aux := 2;
+                END IF;
+                IF ((v_aux * v_array_dv(i)) != 10) THEN 
+                     v_sum := v_sum + (v_aux * v_array_dv(i));
+                END IF;
+                v_aux := v_aux+ 1;
+            END LOOP;
+            IF(MOD(v_sum, 11) != v_array_dv(13)) THEN
+                v_text := 'CNPJ_INVÁLIDO';           
+             END IF;
+         END IF; 
+    END IF;
+    RETURN v_text;
+EXCEPTION
+  WHEN OTHERS THEN
+    RETURN 'OCORREU ALGUMA EXCEÇÃO';
+    --MOD(v_sum_nine, 11)
+    
+END VERIFICA_CNPJ;
+/
+
+
+SELECT CNPJ,
+ VERIFICA_CNPJ(CNPJ)
+FROM DOCUMENTOS;
+
